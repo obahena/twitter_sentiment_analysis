9deb55d513bd5b079fa1cd6c77e36297950a4939
Micaela Gonzalez <micaelagonzalez@Micaelas-MacBook-Air.local>
2018-10-02 00:33:42 -0500

    committing initial changes to sentiment analysis

---
+++ twitter_sentiment_analysis.py
@@ -0,0 +1,16 @@
+
+from TwitterAPI import TwitterAPI
+from textblob import TextBlob
+
+
+
+api = TwitterAPI(consumer_key='4tIauUZj5suBUnT3CvjIwTMrZ',
+  consumer_secret='xV0uvRFQxPaglgQUnQ4cndLcICUfk2WkD7UUpkxhn9gUzjk2xi',
+  access_token_key='233693629-Cdm0oaDJQ1Z7WeIl4IKjhPfjlgwG4ri6P1nfAmZK',
+  access_token_secret='OwnNdBKwV8WJ1abXMLCVOkckBdLsiHp6CjM4VtwCRSwjw')
+
+
+r = api.request('search/tweets', {'q': 'Keenum'})
+
+for item in r:
+    print(item['text'] if 'text' in item else item)
